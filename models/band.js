1b2d2eb4f7376cafdd58018127bab26d670d7b77
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-07-02 13:43:00 -0400

    adding all the models and deleted the extra ones

--- models/band.js
+++ models/band.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 const {
   Model
 } = require('sequelize');
@@ -9,15 +9,25 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ MeetGreet, SetTime }) {
+      // meet and greets
+      Band.hasMany(MeetGreet, {
+        foreignKey: "band_id",
+        as: "meet_greets"
+      })
+
+      // set times 
+      Band.hasMany(SetTime, {
+        foreignKey: "band_id",
+        as: "set_times"
+      })
     }
   }
   Band.init({
     band_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
     name: {
         type: DataTypes.STRING,
@@ -40,6 +50,6 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'Band',
     tableName: 'bands',
     timestamps: false
-  });
-  return Band;
-};
+  })
+  return Band
+}
--- server.js
+++ server.js
@@ -1,48 +1,32 @@
 // DEPENDENCIES
-const express = require('express')	
+const express = require('express')
 const app = express()
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize')
+const bands = require('./controllers/bands_controller')
 
+// CONFIGURATION / MIDDLEWARE
+require('dotenv').config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-// CONFIGURATION / MIDDLEWARE	// CONFIGURATION / MIDDLEWARE
-require('dotenv').config()	
-app.use(express.json())	
-app.use(express.urlencoded({ extended: false }))	
+// ROOT
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the Tour API'
+    })
+})
 
-// ROOT	// ROOT
-app.get('/', (req, res) => {	
-    res.status(200).json({	  
-        message: 'Welcome to the Tour API'	   
-    })	  })
-// CONTROLLERS
-const bandsController = require("./controllers/bands_controller");
-app.use("/bands", bandsController);
-const eventsController = require("./controllers/events_controller");
-app.use("/events", eventsController);
-const stagesConrtoller = require("./controllers/stages_controllers");
-app.use("/stages", stagesConrtoller);
+// CONTROLLERS  
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
 
-// SEQUELIZE CONNECTION
-// const sequelize = new Sequelize({
-//     storage: process.env.PG_URI,
-//     dialect: 'postgres',
-//     username: 'postgres',
-//     password: 'password'
-//   })
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
 
-// SEQUELIZE CONNECTION
-// const sequelize = new Sequelize(process.env.PG_URI)
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
 
-// try {
-//     sequelize.authenticate()
-//     console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
-// } catch(err) {
-//     console.log(`Unable to connect to PG: ${err}`)
-// }
-
-//listen
+// LISTEN
 app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: 
-    ${process.env.PORT}`);
-  }
-)
+    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
+})
