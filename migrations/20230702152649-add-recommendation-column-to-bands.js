82afdc52239776da01723b980b9ea7a7289ec6dc
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-07-02 11:39:53 -0400

    adding help methods

---
+++ migrations/20230702152649-add-recommendation-column-to-bands.js
@@ -0,0 +1,20 @@
+'use strict';
+const { DataTypes } = require('sequelize')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('bands','recomendation', { type: DataTypes.STRING });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.addColumn('bands','recomendation')
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
